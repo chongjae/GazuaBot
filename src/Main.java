02d5863c4dcb34047a3033f2278b7e8fbe427f1f
chongjae.yoo <chongjae.yoo@lge.com>
2017-12-20 17:06:21 +0900

    update main src

--- src/Main.java
+++ src/Main.java
@@ -32,25 +32,15 @@ import com.pengrad.telegrambot.request.SendMessage;
 public class Main {
 
 	public static int minuteThreshold = 5 * 60;// 5 min to sec
-
 	public static float rapidThreshold = 1.035f;
-
 	public static float sellThreshold = 1.02f;
-
 	public static int sellCount = 3;
-
 	public static int logFrequency = 12;
-
 	public static int buyCnt = 0;
-	
 	public static boolean isReallyBuy;
-
 	public static TelegramBot bot = new TelegramBot("");
-
 	public static Logger logger = Logger.getLogger("ChongCoinBot");
-
-	public static Api_Client api = new Api_Client("",
-			""); // connect key, secret key
+	public static Api_Client api = new Api_Client("", ""); // connect key, secret key
 
 	public static void main(String args[]) {
 
@@ -78,112 +68,62 @@ public class Main {
 		coinName.put("EOS ", "이오스");
 
 		try {
-
 			fh = new FileHandler("./ChongCoinBot.log");
-
 			logger.addHandler(fh);
-
 			SimpleFormatter formatter = new SimpleFormatter();
-
 			fh.setFormatter(formatter);
-
 		} catch (Exception e) {
 		}
-
 		while (true) {
-
 			try {
 				String result = api.callApi("/public/ticker/ALL", rgParams);
-
 				JSONParser parser = new JSONParser();
-
 				JSONObject object = (JSONObject) parser.parse(result);
-
 				object = (JSONObject) object.get("data");
-
 				@SuppressWarnings("unchecked")
-
 				Set<String> keys = object.keySet();
-
 				HashMap<String, String> rapidSet = new HashMap<>();
 				HashMap<String, String> sellSet = new HashMap<>();
-
 				long date = 0;
-
 				for (String key : keys) {
-
 					if (key.equals("date")) {
-
 						date = Long.parseLong(object.get(key).toString());
-
 					} else {
-
 						if (coins.containsKey(key)) {
-
 							float curPrice = Float
-
 									.parseFloat(((JSONObject) object.get(key)).get("buy_price").toString());
-
 							CoinInfo coinInfo = coins.get(key);
-
 							if (curPrice > coinInfo.maxPrice) {
-
 								coinInfo.maxPrice = curPrice;
-
 								coinInfo.maxDate = date;
-
 							} else if (curPrice < coinInfo.minPrice) {
-
 								coinInfo.minPrice = curPrice;
-
 								coinInfo.minDate = date;
-
 							}
-
 							long diff = date - coinInfo.minDate;
-
 							long diffSecond = (diff / 1000) % 60 + (diff / 1000) / 60 * 60; // second
-
 							if (diffSecond <= minuteThreshold) {
-
 								if (curPrice / coinInfo.minPrice >= rapidThreshold) {
-
 									float rate = curPrice / coinInfo.minPrice;
-
 									rapidSet.put(key, coinName.get(key) + "이 급등하였습니다. " + coinInfo.minPrice + " -> "
-											+ curPrice + "("
-
-											+ "(" + rate + ")");
-
+											+ curPrice + "(" + "(" + rate + ")");
 									coinInfo.updateMin(date, curPrice);
-
 									if (coinInfo.buyPrice == 0) {
 										coinInfo.buyPrice = curPrice;
 										if (isReallyBuy) {
 											buyCoin(coinInfo);
 										}
 									}
-
 								}
-
 							} else {
-
 								System.out.println("update min by over 5 min");
-
 								coinInfo.updateMin(date, curPrice);
-
 							}
-
 							diff = date - coinInfo.maxDate;
-
 							diffSecond = (diff / 1000) % 60 + (diff / 1000) / 60 * 60; // second
-
 							if (diffSecond <= minuteThreshold) {
-
 								if (coinInfo.maxPrice / curPrice >= sellThreshold && coinInfo.buyPrice != 0) {
-
 									float rate = curPrice / coinInfo.buyPrice;
-
 									if (rate <= 1.003f && coinInfo.sellCnt <= sellCount) {
 										sellSet.put(key,
 												coinName.get(key) + "를 " + coinInfo.buyPrice + "에 매수하여, " + curPrice
@@ -199,107 +139,62 @@ public class Main {
 											sellCoin(coinInfo);
 										}
 									}
-
 									coinInfo.updateMax(date, curPrice);
-
 								}
-
 							} else {
-
 								System.out.println("update max by over 5 min");
-
 								coinInfo.updateMax(date, curPrice);
 								if (coinInfo.buyPrice != 0) {
 									coinInfo.sellCnt = 0;
 								}
 							}
-
 						} else {
-
 							CoinInfo coinInfo = new CoinInfo(key, date,
-
 									Float.valueOf(((JSONObject) object.get(key)).get("buy_price").toString()));
-
 							coins.put(key, coinInfo);
-
 						}
-
 					}
-
 				}
-
 				if (rapidSet.size() > 0) {
-
 					String ret = "";
-
 					for (String rapidKey : rapidSet.keySet()) {
-
 						ret += rapidSet.get(rapidKey) + "\n";
-
 					}
-
 					sendMsgToTelegram(ret, false);
-
 				}
 				if (sellSet.size() > 0) {
 					String ret = "";
-
 					for (String rapidKey : sellSet.keySet()) {
-
 						ret += rapidKey + " : " + sellSet.get(rapidKey) + "\n";
-
 					}
-
 					sendMsgToTelegram(ret, true);
 				}
-
 				Thread.sleep(4 * 1000);
-
 				logCnt++;
-
 				if (logCnt == logFrequency) {
-
 					logCnt = 0;
-
 					logger.info("System is running....");
-
 				}
-
 			} catch (Exception e) {
-
 				logger.info(e.getMessage());
-
 			}
-
 		}
 	}
 
 	public static void sendMsgToTelegram(String msg, boolean toMe) {
-
 		logger.info("Send msg to Telegram : " + msg);
-
 		Connection con = null;
-
 		Statement stat = null;
-
 		try {
-
 			Class.forName("org.sqlite.JDBC");
-
 			con = DriverManager.getConnection("jdbc:sqlite:user.db");
-
 			stat = con.createStatement();
-
 		} catch (Exception e) {
-
 			e.printStackTrace();
-
 		}
-
 		try {
-
 			ResultSet ret = stat.executeQuery("select * from user");
-
+			boolean isDisableBuy = false;
 			while (ret.next()) {
 				String userId = ret.getString("user");
 				if (toMe) {
@@ -307,19 +202,16 @@ public class Main {
 						continue;
 					}
 				}
-				if("9999".equals(userId)) {
-					isReallyBuy = false;
+				if ("9999".equals(userId)) {
+					isDisableBuy = true;
 				}
 				SendMessage request = new SendMessage(userId, msg).parseMode(ParseMode.HTML);
 				bot.execute(request);
 			}
-
+			isReallyBuy = !isDisableBuy;
 		} catch (SQLException e) {
-
 			e.printStackTrace();
-
 		}
-
 	}
 
 	public static void buyCoin(CoinInfo coin) {
@@ -340,13 +232,12 @@ public class Main {
 			result = api.callApi("/trade/market_buy", rgParams);
 			JSONParser parser = new JSONParser();
 			JSONArray array = (JSONArray) ((JSONObject) parser.parse(result)).get("data");
-
 			for (int i = 0; i < array.size(); i++) {
 				JSONObject obj = (JSONObject) array.get(i);
 				coin.buyPrice = Float.valueOf(obj.get("price").toString());
 			}
 			if (coin.isReallyBuy) {
-				buyCnt++;				
+				buyCnt++;
 				coin.isReallyBuy = true;
 			}
 		} catch (Exception e) {
@@ -368,7 +259,7 @@ public class Main {
 				} catch (Exception e1) {
 					try {
 						unit = unit.substring(0, unit.indexOf(".") + 3);
-					}catch (Exception e2) {
+					} catch (Exception e2) {
 					}
 				}
 			}
@@ -408,53 +299,31 @@ public class Main {
 	}
 
 	public static class CoinInfo {
-
 		public String key;
-
 		public long minDate;
-
 		public long maxDate;
-
 		public float minPrice;
-
 		public float maxPrice;
-
 		public float buyPrice;
-
 		public int sellCnt;
-
 		public boolean isReallyBuy = false;
 
 		public CoinInfo(String key, long date, float price) {
-
 			this.key = key;
-
 			minDate = date;
-
 			maxDate = date;
-
 			minPrice = price;
-
 			maxPrice = price;
-
 		}
 
 		public void updateMax(long date, float price) {
-
 			maxDate = date;
-
 			maxPrice = price;
-
 		}
 
 		public void updateMin(long date, float price) {
-
 			minDate = date;
-
 			minPrice = price;
-
 		}
-
 	}
-
 }
